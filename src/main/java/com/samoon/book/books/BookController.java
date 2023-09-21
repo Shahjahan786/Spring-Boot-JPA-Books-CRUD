e6b2ef00f6c7d366e18d25329e230a0d9deb0823
= <=>
2023-09-21 18:21:24 +0500

    Updated Book Controller

--- src/main/java/com/samoon/book/books/BookController.java
+++ src/main/java/com/samoon/book/books/BookController.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("books")
+@CrossOrigin("*")
 public class BookController {
     @Autowired
     BookRepository bookRepository;
@@ -25,7 +27,7 @@ public class BookController {
     }
 
     @PostMapping
-    public String save(Book book){
+    public String save(@RequestBody Book book){
         bookRepository.save(book);
         return "Book saved!";
     }
